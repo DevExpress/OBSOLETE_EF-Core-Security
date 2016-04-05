62141fda3fac4edda0e43617c7ec6a640b09c2b5
unfoo <alexander.neroslavskiy@devexpress.com>
2016-04-05 16:49:16 +0300

    Beginning of the EF Core Security Demo for Android (OData client)

---
+++ EFCore Security Applications/EFCoreSecurityAndroidDemo/app/src/main/java/com/devexpress/efcoresecurity/efcoresecuritydemo/LoginActivity.java
@@ -0,0 +1,76 @@
+package com.devexpress.efcoresecurity.efcoresecuritydemo;
+
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Spinner userNameSpinner;
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        context = this;
+
+        userNameSpinner = (Spinner) findViewById(R.id.userNameSpinner);
+
+        String[] data = {"John", "Admin"};
+
+        ArrayAdapter<String> userNameSpinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, data);
+        userNameSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        userNameSpinner.setAdapter(userNameSpinnerAdapter);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentUserName = userNameSpinner.getSelectedItem().toString();
+                Toast.makeText(context, "Logged in as " +currentUserName, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
