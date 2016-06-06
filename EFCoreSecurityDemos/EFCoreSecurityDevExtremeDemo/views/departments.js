f39f0a63d77feaf3f54e93d629f3d32edc3f4137
Pavel Tikhomirov <pavel.tikhomirov@devexpress.com>
2016-06-06 18:27:56 +0300

    add grid to all entity

--- EFCoreSecurityDemos/EFCoreSecurityDevExtremeDemo/views/departments.js
+++ EFCoreSecurityDemos/EFCoreSecurityDevExtremeDemo/views/departments.js
@@ -1,6 +1,41 @@
 ﻿TestApp.Departments = function (params) {
     var viewModel = {
-        dSource: new DevExpress.data.DataSource(TestApp.db.sampleData.Departments)
+        dSource: new DevExpress.data.DataSource(TestApp.db.sampleData.Departments),
+        dataGridOptions: {
+            dataSource: new DevExpress.data.DataSource(TestApp.db.sampleData.Departments),
+            paging: {
+                pageSize: 10
+            },
+            pager: {
+                showPageSizeSelector: true,
+                allowedPageSizes: [5, 10, 20],
+                showInfo: true
+            },
+
+            columns: ["Id", "Title", "Office"],
+
+            onRowClick: function (info) {
+                TestApp.app.navigate('departments_detail/' + info.data.Id);
+            },
+
+            onCellPrepared: function (options) {
+                var fieldData = options.value,
+                    fieldHtml = "",
+                    model = options.model;
+
+                if (options.rowType == "data") {
+                    var columnCaption = options.column.caption;
+                    var blockedMembers = options.data.BlockedMembers;
+                    if (blockedMembers.indexOf(columnCaption) >= 0) {
+                        options.cellElement.addClass("protected");
+                        fieldHtml += "<span>Protected</span>";
+                    } else {
+                        fieldHtml = fieldData.value;
+                    }
+                    options.cellElement.html(fieldHtml);
+                }
+            },
+        }
     };
     return viewModel;
 };
--- EFCoreSecurityDemos/EFCoreSecurityDevExtremeDemo/views/tasks.js
+++ EFCoreSecurityDemos/EFCoreSecurityDevExtremeDemo/views/tasks.js
@@ -1,6 +1,41 @@
 ﻿TestApp.Tasks = function (params) {
     var viewModel = {
-        dSource: new DevExpress.data.DataSource(TestApp.db.sampleData.Tasks)
+        dSource: new DevExpress.data.DataSource(TestApp.db.sampleData.Tasks),
+        dataGridOptions: {
+            dataSource: new DevExpress.data.DataSource(TestApp.db.sampleData.Tasks),
+            paging: {
+                pageSize: 10
+            },
+            pager: {
+                showPageSizeSelector: true,
+                allowedPageSizes: [5, 10, 20],
+                showInfo: true
+            },
+
+            columns: ["Id", "Description", "Note", "StartDate", "DateCompleted", "PercentCompleted"],
+
+            onRowClick: function (info) {
+                TestApp.app.navigate('tasks_detail/' + info.data.Id);
+            },
+
+            onCellPrepared: function (options) {
+                var fieldData = options.value,
+                    fieldHtml = "",
+                    model = options.model;
+
+                if (options.rowType == "data") {
+                    var columnCaption = options.column.caption;
+                    var blockedMembers = options.data.BlockedMembers;
+                    if (blockedMembers.indexOf(columnCaption) >= 0) {
+                        options.cellElement.addClass("protected");
+                        fieldHtml += "<span>Protected</span>";
+                    } else {
+                        fieldHtml = fieldData.value;
+                    }
+                    options.cellElement.html(fieldHtml);
+                }
+            },
+        }
     };
     return viewModel;
 };
